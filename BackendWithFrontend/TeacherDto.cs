eb16084bffe0a810fcf5b4b9470c8400c3c720ee
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2024-01-12 21:03:40 +0500

    changed

--- BackendWithFrontend/TeacherDto.cs
+++ BackendWithFrontend/TeacherDto.cs
@@ -5,8 +5,8 @@
         public string name { get; set; }
         public string description { get; set; }
         public string address { get; set; }
-        public DateTime dateOfBirth { get; set; }
-        public DateTime dateOfRegister { get; set; }
+        public string dateOfBirth { get; set; }
+        public string dateOfRegister { get; set; }
         public string phone { get; set; }
         public string email { get; set; }
         public string telegramUserName { get; set; }
